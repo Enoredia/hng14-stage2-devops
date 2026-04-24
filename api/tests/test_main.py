4979910dfe4d293c984f58bb00933e85f40e9f72
Enoredia <38014615+Enoredia@users.noreply.github.com>
2026-04-24 20:50:44 +0100

    Reorganize imports in test_main.py

--- api/tests/test_main.py
+++ api/tests/test_main.py
@@ -1,11 +1,12 @@
 import sys
 from pathlib import Path
-from fastapi.testclient import TestClient
-
-import main
 
 
 sys.path.insert(0, str(Path(__file__).resolve().parents[1]))
+
+
+from fastapi.testclient import TestClient
+import main
 
 
 client = TestClient(main.app)
