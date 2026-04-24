e07a416fbe2904e58bd27523ee9ea1b8d02e9584
Enoredia <38014615+Enoredia@users.noreply.github.com>
2026-04-24 20:59:28 +0100

    Refactor import statements in test_main.py
    Reorder import statements for better organization.

--- api/tests/test_main.py
+++ api/tests/test_main.py
@@ -1,12 +1,11 @@
 import sys
 from pathlib import Path
+
 from fastapi.testclient import TestClient
 
-
-import main
-
-
 sys.path.insert(0, str(Path(__file__).resolve().parents[1]))
+
+import main  # noqa: E402
 
 
 client = TestClient(main.app)
